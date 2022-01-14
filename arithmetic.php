57322aec153a1f20e631924be9e9646ad7b91c2a
JesminNancy <jesminnancy@hotmail.com>
2022-01-14 21:37:19 +0600

    done

---
+++ arithmetic.php
@@ -0,0 +1,34 @@
+<?php
+echo "<h2>Example Of Arithmetic Operators:</h2>";
+
+// Addition
+$num1 = 10;
+$num2 = 30;
+
+$add = $num1 + $num2;
+
+echo "The Sum of Two no. is: $add<br>";
+
+// Subtraction
+$num3 = 100;
+$num4 = 40;
+
+$sub = $num3 - $num4;
+
+echo "The Difference of Two no. is: $sub<br>";
+
+// Multiplication
+$num5 = 5;
+$num6 = 5;
+
+$multi = $num5 * $num6;
+
+echo "The Multiplication of Two no. is: $multi<br>";
+
+// Division
+$num7 = 30;
+$num8 = 10;
+
+$division = $num7 / $num8;
+
+echo "The Division of Two no. is: $division";
