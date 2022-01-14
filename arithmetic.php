99c12b1234cf1aa47c25ae66a0dad86a989fc7d2
JesminNancy <jesminnancy@hotmail.com>
2022-01-14 23:00:50 +0600

    done

--- arithmetic.php
+++ arithmetic.php
@@ -1,7 +1,7 @@
 <?php
 echo "<h2>Example Of Arithmetic Operators:</h2>";
 
-// Addition
+// Addition operator
 $num1 = 10;
 $num2 = 30;
 
@@ -9,7 +9,7 @@ $add = $num1 + $num2;
 
 echo "The Sum of Two no. is: $add<br>";
 
-// Subtraction
+// Subtraction operator
 $num3 = 100;
 $num4 = 40;
 
@@ -17,7 +17,7 @@ $sub = $num3 - $num4;
 
 echo "The Difference of Two no. is: $sub<br>";
 
-// Multiplication
+// Multiplication operator
 $num5 = 5;
 $num6 = 5;
 
@@ -25,10 +25,28 @@ $multi = $num5 * $num6;
 
 echo "The Multiplication of Two no. is: $multi<br>";
 
-// Division
+// Division operator
 $num7 = 30;
 $num8 = 10;
 
 $division = $num7 / $num8;
 
-echo "The Division of Two no. is: $division";
+echo "The Division of Two no. is: $division<br>";
+
+// Modulus operator
+
+$num9 = 36;
+$num10 = 5;
+
+$modulus = $num9 % $num10;
+
+echo "The Modulus of Two no. is: $modulus<br>";
+
+//Exponentiation operator
+
+$base = 5;
+$power = 3;
+
+$powerOfNumber = pow($base, $power);
+
+echo "The Exponentiation of Two no. is $powerOfNumber";
