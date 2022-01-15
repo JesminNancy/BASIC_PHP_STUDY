824a4bad678b5c5df82b30d6020a7f1c1df83c80
JesminNancy <jesminnancy@hotmail.com>
2022-01-15 11:57:40 +0600

    done

---
+++ ifelse.php
@@ -0,0 +1,20 @@
+<?php
+
+$day = "Monday";
+
+if ($day == "sunday") {
+  echo "Today is Holiday<br>";
+} else {
+  echo "Today is not Holiday<br>";
+}
+
+
+$username = "Nancy";
+
+if ($username == "Nancy") {
+
+  echo "Login Successfull";
+} else {
+
+  echo "Login Failed";
+}
