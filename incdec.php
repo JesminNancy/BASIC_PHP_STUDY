46f0a464cb045584ae30a32c97c10a9b5664b4e6
JesminNancy <jesminnancy@hotmail.com>
2022-01-16 13:02:39 +0600

    done

---
+++ incdec.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = 2;
+$b = 5;
+
+
+/* echo $a++ . "<br>";
+echo $a;
+
+echo ++$b . "<br>"; */
+
+/* echo $a-- . "<br>";
+echo --$b . "<br>"; */
+
+
+/* $x = 3;
+
+echo $x++  +  ++$x; */
+
+
+$c = $a++  +  $a++  +  ++$a;
+
+echo $c;
