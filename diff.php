a233676056d5c3766072bb11a9e2d24dd8a6dfe5
JesminNancy <jesminnancy@hotmail.com>
2022-01-20 11:48:15 +0600

    done

---
+++ diff.php
@@ -0,0 +1,18 @@
+<?php
+
+$num1 = 10;
+$num2 = "10";
+
+/* if($num1 == $num2){
+    echo "Both the values are same";
+}else{
+    echo "Values are same but data types are diff.";
+} */
+
+if($num1 === $num2){
+    echo "Both the values are same";
+}else{
+    echo "Values are same but data types are diff.";
+}
+
+?>
