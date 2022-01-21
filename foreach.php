56287eb86affc7f67523b1d914b364832cdd0065
JesminNancy <jesminnancy@hotmail.com>
2022-01-21 19:48:06 +0600

    done

---
+++ foreach.php
@@ -0,0 +1,12 @@
+<?php
+
+$students = array("Jesmin", "Akther", "Nancy", "Radifa", "Ripon", "Anika");
+
+echo "<ol>";
+foreach($students as $names){
+    echo  "<li>" . $names . "</li>" ;
+}
+
+echo "<ol>";
+
+?>
