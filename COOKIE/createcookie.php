c0e35a1dbfbe0b8ad992b2c697dac4e06e768389
JesminNancy <jesminnancy@hotmail.com>
2022-01-24 20:23:12 +0600

    done

---
+++ COOKIE/createcookie.php
@@ -0,0 +1,8 @@
+<?php
+
+setcookie('youtubeuser', 'nancy', time()+600); 
+
+// setcookie(name, value, expire, path, domain, secure, httponly);
+
+
+?>
---
+++ COOKIE/updatecookie.php
@@ -0,0 +1,6 @@
+<?php
+
+setcookie('youtubeuser', 'Jesmin', time()+86400); 
+
+
+?>
